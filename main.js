9a3ff53689b9dc09b16ac0a7ceef80725aa72921
mohamedalielhag <mohamed.ali.elhag1@gmail.com>
2021-02-11 11:34:01 +0800

    Files

---
+++ main.js
@@ -0,0 +1,10 @@
+$('#navbar a, .btn') .on('click', function(event){
+    if(this.hash !==''();
+    const hash = this.hash;
+    $('html, body').animate(
+        scrollTop:$(hash).offset().top-80
+},
+800
+    );
+}
+});
